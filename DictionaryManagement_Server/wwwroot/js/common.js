c8f020c028a492e0944474ed9f3cb6b973aedf30
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2023-05-06 20:59:46 +0700

    Подключение toastr

---
+++ DictionaryManagement_Server/wwwroot/js/common.js
@@ -0,0 +1,34 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, "Операция выполнена успешно", { timeOut: 5000 });
+    }
+    if (type === "error") {
+        toastr.error(message, "Ошибка выполнения операции", { timeOut: 5000 });
+    }
+}
+
+
+window.ShowSwal = (type, message) => {
+    if (type === "success") {
+        Swal.fire(
+            'Успешная операция!',
+            message,
+            'success'
+        )
+    }
+    if (type === "error") {
+        Swal.fire(
+            'Ошибка операции!',
+            message,
+            'error'
+        )
+    }
+}
+
+function ShowDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('show');
+}
+
+function HideDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('hide');
+}
