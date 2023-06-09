8aecceea69f501f3e0323044e0271ddbe2ea622e
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2023-06-10 04:20:49 +0700

    Изменение порта для https по умолчанию.

--- DictionaryManagement_Server/Program.cs
+++ DictionaryManagement_Server/Program.cs
@@ -2,8 +2,10 @@ using DictionaryManagement_Business.Repository;
 using DictionaryManagement_Business.Repository.IRepository;
 using DictionaryManagement_Common;
 using DictionaryManagement_DataAccess.Data.IntDB;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Radzen;
@@ -14,6 +16,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+builder.WebHost.UseUrls("http://localhost:7776", "https://localhost:7777");
+builder.Services.AddHttpsRedirection(options => options.HttpsPort = 7777);
+
 
 SD.AppFactoryMode = SD.FactoryMode.KOS;
 if (builder.Configuration.GetValue<string>("FactoryMode") == "NKNH")
@@ -50,6 +56,7 @@ builder.Services.AddScoped<ContextMenuService>();
 
 
 
+
 System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
 System.Diagnostics.FileVersionInfo fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
 SD.AppVersion = fvi.FileVersion;
@@ -63,6 +70,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 
