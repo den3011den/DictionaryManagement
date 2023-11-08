dd055323430ee27f7c4665bbf95acddd41f7f1b3
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2023-11-09 03:15:30 +0700

    В Журнал экземпляров отчётов добавлен вывод в таблицу производства с возможнотью фильтра. Добавлен фильтр к датам загрузки/выгрузки и по фамилиям кто скачивал/загружал экземпляр отчёта.

---
+++ DictionaryManagement_Server/Extensions/RadzenDataGridApp.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using Radzen.Blazor;
+
+namespace DictionaryManagement_Server.Extensions
+{
+    public class RadzenDataGridApp<TItem> : RadzenDataGrid<TItem>
+    {
+        public RadzenDataGridApp() : base()
+        {
+
+            base.AndOperatorText = "И";
+            base.OrOperatorText = "ИЛИ";
+            base.StartsWithText = "Начинается с";
+            base.ApplyFilterText = "Применить";
+            base.ClearFilterText = "Очистить";
+            base.ContainsText = "Содержит";
+            base.EndsWithText = "Кончается на";
+            base.DoesNotContainText = "Не содержит";
+            base.EqualsText = "Равно";
+            base.GreaterThanOrEqualsText = "Больше или равно";
+            base.GreaterThanText = "Больше чем";
+            base.IsEmptyText = "Пусто (пустая строка)";
+            base.IsNotEmptyText = "Не пусто (не пустая строка)";
+            base.LessThanOrEqualsText = "Меньше или равно";
+            base.LessThanText = "Меньше чем";
+            base.IsNotNullText = "Не пусто (не NULL)";
+            base.IsNullText = "Пусто (NULL)";
+            base.NotEqualsText = "Не равно";
+            base.FilterText = "Фильтр";
+        }
+    }
+}
