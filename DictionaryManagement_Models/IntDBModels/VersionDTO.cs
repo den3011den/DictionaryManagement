9ca4903e746b204718d7543254d227eaf1f9cb57
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2023-11-24 05:28:56 +0700

    Версия 0.10.5.8. Рабочий коммит. MVP архивов, витрин SAP и журнала событий. Исправление ошибок. Реализация части поженаний.

--- DictionaryManagement_Models/IntDBModels/VersionDTO.cs
+++ DictionaryManagement_Models/IntDBModels/VersionDTO.cs
@@ -12,7 +12,7 @@ namespace DictionaryManagement_Models.IntDBModels
     public class VersionDTO
     {
 
-        [Display(Name = "ИД записи")]
+        [Display(Name = "ИД записи")] 
         [Required(ErrorMessage = "ИД обязателен")]
         public int Id { get; set; }
 
