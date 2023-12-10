712a856f9884af8458eea49993f5f48b8fec1c66
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2023-12-11 05:12:55 +0700

    Фильтры по числовому типу по Value в архивах. Переделка фильтров в ReportTemplate.

---
+++ DictionaryManagement_Server/Extensions/StringExtendion.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+public static class StringExtension
+{
+    public static string HideSlash(this string value)
+    {        
+        return value.Replace("\\", "__123455432__");
+    }
+
+    public static string UnhideSlash(this string value)
+    {
+        return value.Replace("__123455432__", "\\");
+    }
+}
