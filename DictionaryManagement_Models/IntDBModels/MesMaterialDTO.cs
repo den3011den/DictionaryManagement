7678ea77050e67f9f83d638bc986fdc688133c3d
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2023-12-22 13:51:51 +0700

    Логирование изменений материалов SAP и MES.

--- DictionaryManagement_Models/IntDBModels/MesMaterialDTO.cs
+++ DictionaryManagement_Models/IntDBModels/MesMaterialDTO.cs
@@ -5,26 +5,30 @@ namespace DictionaryManagement_Models.IntDBModels
 {
     public class MesMaterialDTO
     {
+        [ForLogAttribute(NameProperty = "поле \"ИД записи\"")]
         [Display(Name = "ИД записи")]
         [Required(ErrorMessage = "ИД записи является обязательным для заполнения полем")]
         public int Id { get; set; }
 
-
+        [ForLogAttribute(NameProperty = "поле \"Код материала\"")]
         [Required(ErrorMessage = "Код материала MES является обязательным для заполнения полем")]
-        [Display(Name = "Наименование материала MES")]
-        [MaxLength(100, ErrorMessage = "Наименование материала MES не может быть больше 100 символов")]
+        [Display(Name = "Код материала MES")]
+        [MaxLength(100, ErrorMessage = "Код материала MES не может быть больше 100 символов")]
         public string Code { get; set; } = string.Empty;
 
+        [ForLogAttribute(NameProperty = "поле \"Наименование\"")]
         [Required(ErrorMessage = "Наименование материала MES является обязательным для заполнения полем")]
         [Display(Name = "Наименование материала MES")]
         [MaxLength(250, ErrorMessage = "Наименование материала MES не может быть больше 250 символов")]
         public string Name { get; set; } = string.Empty;
 
+        [ForLogAttribute(NameProperty = "поле \"Сокр. наименование\"")]
         [Required(ErrorMessage = "Сокращённое наименование материала MES является обязательным для заполнения полем")]
         [Display(Name = "Сокращённое наименование материала MES")]
         [MaxLength(100, ErrorMessage = "Сокращённое наименование материала MES не может быть больше 100 символов")]
         public string ShortName { get; set; } = string.Empty;
 
+        [ForLogAttribute(NameProperty = "поле \"Архив\"")]
         [Display(Name = "В архиве")]
         public bool IsArchive { get; set; }
 
--- DictionaryManagement_Models/IntDBModels/SapMaterialDTO.cs
+++ DictionaryManagement_Models/IntDBModels/SapMaterialDTO.cs
@@ -5,26 +5,31 @@ namespace DictionaryManagement_Models.IntDBModels
 {
     public class SapMaterialDTO
     {
+        [ForLogAttribute(NameProperty = "поле \"ИД записи\"")]
         [Display(Name = "ИД записи")]
         [Required(ErrorMessage = "ИД записи является обязательным для заполнения полем")]
         public int Id { get; set; }
 
 
+        [ForLogAttribute(NameProperty = "поле \"Код материала\"")]
         [Required(ErrorMessage = "Код материала SAP является обязательным для заполнения полем")]
-        [Display(Name = "Наименование материала SAP")]
-        [MaxLength(100, ErrorMessage = "Наименование материала SAP не может быть больше 100 символов")]
+        [Display(Name = "Код материала SAP")]
+        [MaxLength(100, ErrorMessage = "Код материала SAP не может быть больше 100 символов")]
         public string Code { get; set; } = string.Empty;
 
+        [ForLogAttribute(NameProperty = "поле \"Наименование\"")]
         [Required(ErrorMessage = "Наименование материала SAP является обязательным для заполнения полем")]
         [Display(Name = "Наименование материала SAP")]
         [MaxLength(250, ErrorMessage = "Наименование материала SAP не может быть больше 250 символов")]
         public string Name { get; set; } = string.Empty;
 
+        [ForLogAttribute(NameProperty = "поле \"Сокр. наименование\"")]
         [Required(ErrorMessage = "Сокращённое наименование материала SAP является обязательным для заполнения полем")]
         [Display(Name = "Сокращённое наименование материала SAP")]
         [MaxLength(100, ErrorMessage = "Сокращённое наименование материала SAP не может быть больше 100 символов")]
         public string ShortName { get; set; } = string.Empty;
 
+        [ForLogAttribute(NameProperty = "поле \"Архив\"")]
         [Display(Name = "В архиве")]
         public bool IsArchive { get; set; }
 
