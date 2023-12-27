c32104f45566837106bc8ca3245671fbe0a6893d
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2023-12-27 18:49:11 +0700

    Логирование изменений в справочниках Типов событий и Типов шаблонов отчётов.

--- DictionaryManagement_Models/IntDBModels/ReportTemplateTypeDTO.cs
+++ DictionaryManagement_Models/IntDBModels/ReportTemplateTypeDTO.cs
@@ -5,21 +5,25 @@ namespace DictionaryManagement_Models.IntDBModels
 {
     public class ReportTemplateTypeDTO
     {
+        [ForLogAttribute(NameProperty = "поле \"ИД записи\"")]
         [Display(Name = "Ид записи")]
         [Required(ErrorMessage = "Код типа шаблона отчёта является обязательным для заполнения полем")]
         public int Id { get; set; }
 
+        [ForLogAttribute(NameProperty = "поле \"Наименование\"")]
         [Required(ErrorMessage = "Наименование типа шаблона отчёта является обязательным для заполнения полем")]
         [Display(Name = "Наименование типа шаблона отчёта")]
         [MaxLength(100, ErrorMessage = "Наименование типа шаблона отчёта не может быть больше 100 символов")]
         public string Name { get; set; } = string.Empty;
 
+        [ForLogAttribute(NameProperty = "поле \"Архив\"")]
         [Display(Name = "В архиве")]
         public bool IsArchive { get; set; }
 
         [Display(Name = "Требуется авторасчёт")]
         public bool? NeedAutoCalc { get; set; }
 
+        [ForLogAttribute(NameProperty = "поле \"Доступен для настройки авторасчёта\"")]
         [Display(Name = "Доступен для настройки авторасчёта")]
         public bool? CanAutoCalc { get; set; }
 
@@ -43,6 +47,11 @@ namespace DictionaryManagement_Models.IntDBModels
             {
                 ToStringId = value;
             }
+        }
+
+        public override string ToString()
+        {
+            return Name;
         }
     }
 }
